ffb27d96b6d0ad851016eac7d880c4d159bb7f34
Byron Custodio <byron.custodio@gmail.com>
2023-01-23 20:23:35 -0800

    - Removed use of assigned env values

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,20 +1,11 @@
-import { ConnectionStringParser } from "connection-string-parser";
-
-const connectionStringParser = new ConnectionStringParser({
-  scheme: "mysql",
-  hosts: []
-});
-
-const connectionString = connectionStringParser.parse(process.env.DATABASE_URL);
-
 export default ({ env }) => ({
     connection: {
       client: 'mysql',
       connection: {
-        host: env('DATABASE_HOST', connectionString.hosts[0].host),
-        database: env('DATABASE_NAME', connectionString.scheme),
-        user: env('DATABASE_USERNAME', connectionString.username),
-        password: env('DATABASE_PASSWORD', connectionString.password),
+        host: env('DATABASE_HOST'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: {
           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
         },
